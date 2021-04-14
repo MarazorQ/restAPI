45cc3e281f0875c722bdfa3bf5c892d0d93e8ff5
Marazorq <shuratov2001@mail.ru>
2021-04-14 20:29:47 +0300

    added and conection DB

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express"
+import mongoose from "mongoose"
 
+const DB_URL = `mongodb+srv://user:user@cluster0.svz2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = 5000
 
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.status(200).json('servers')
+    console.log(req.query)
+    res.status(200).json('GET RESPONSE')
 })
 
-app.listen(PORT, ()=>{
-    console.log("server is run")
-})
+async function startApp(){
+    try{
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, ()=>{
+            console.log("server is run")
+        })
+    }catch (e){
+        console.log(e)
+    }
+}
+
