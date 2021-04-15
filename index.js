22d0362dc7201d053b3cd3ab709a451070e4feea
Marazorq <shuratov2001@mail.ru>
2021-04-15 14:42:44 +0300

    crated error logs

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 import express from "express"
 import mongoose from "mongoose"
+import Post from "./Post.js"
 
 const DB_URL = `mongodb+srv://user:user@cluster0.svz2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = 5000
 
 const app = express()
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    console.log(req.query)
-    res.status(200).json('GET RESPONSE')
+app.post('/', async (req,res)=>{
+    try{
+        const {title, content, author} = req.body// дисттруктуризация
+        const post = await Post.create({title, content, author})
+        res.json(post)
+    }catch (e){
+        res.status(500).json(e)
+    }
 })
 
 async function startApp(){
     try{
         await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
         app.listen(PORT, ()=>{
-            console.log("server is run")
+            console.log(`server is run on ${PORT}`)
         })
     }catch (e){
         console.log(e)
