a3a66400ba42115bc8afc9033b2854c831426e23
Marazorq <shuratov2001@mail.ru>
2021-04-15 15:48:51 +0300

    created get,put,delete methods

--- PostController.js
+++ PostController.js
@@ -10,6 +10,51 @@ class PostController{
             res.status(500).json(e)
         }
     }
+    async getAll(req, res){
+        try{
+            const posts = await Post.find()
+            return res.json(posts)
+        }catch (e){
+            res.status(500).json(e)
+        }
+    }
+    async getOne(req, res){
+        try{
+            const {id} = req.params
+            if (!id){
+                res.status(400).json({message: "Укажите ID"})
+            }
+            const post = await Post.findById(id)
+            return res.json(post)
+        }catch (e){
+            res.status(500).json(e)
+        }
+    }
+    async update(req, res){
+        try{
+            const post = req.body
+            if (!post._id){
+                res.status(400).json({message: "Укажите ID"})
+            }
+            const updatePost = await Post.findByIdAndUpdate(post._id, post, {new: true})// new чтобы вернулась обновленная версия поста
+            return res.json(updatePost)
+        }catch (e){
+            res.status(500).json(e)
+        }
+    }
+    async delete(req, res){
+        try{
+            const {id} = req.params
+            if (!id){
+                res.status(400).json({message: "Укажите ID"})
+            }
+            const post = await Post.findByIdAndDelete(id)
+            return res.json(post)
+        }catch (e){
+            res.status(500).json(e)
+        }
+    }
+
 }
 
 export default new PostController()
