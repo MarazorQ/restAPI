a2cf9da98100dd518bbe36746f0aaa496c352ef3
Marazorq <shuratov2001@mail.ru>
2021-04-21 15:49:01 +0300

    create reg and added bcrypt

--- authController.js
+++ authController.js
@@ -1,24 +1,43 @@
 import Users from "./Users.js"
 import Role from "./Role.js"
+import bcrypt from 'bcryptjs'
 
 class authController{
     async registration(req, res){
         try{
+            const {username, password} = req.body
+            const candidate = await Users.findOne({username})
 
+            if (candidate){
+                return res.status(400).json({message: "This name is alredy!"})
+            }
+
+            const salt = bcrypt.genSaltSync(4);
+            const hashPassword = bcrypt.hashSync(password, salt)
+
+            const userRole = await Role.findOne({value: "USER"})
+
+            const user = new Users({username, password: hashPassword, roles: [userRole.value]})
+            await user.save()
+            
+            return res.json({message: "Reg sacs!"})
         }catch (e){
-
+            console.log(e)
+            res.status(400).json({message: "Reg err"})
         }
     }
     async login(req, res){
         try{
 
         }catch (e){
-            
+            console.log(e)
+            res.status(400).json({message: "Log err"})
         }
     }
     async getUsers(req, res){
         try{
-            res.json("get users")
+            
+            res.json("fddfd")
         }catch (e){
             
         }
