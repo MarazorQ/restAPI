40b36d2513534ffecfabc05a1811c27368b95c38
Marazorq <shuratov2001@mail.ru>
2021-04-21 15:03:00 +0300

    added controller for new router

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,10 @@
-const Router = require('express')
+import authController from "./authController.js"
+import Router from "express"
+
 const authRouter = new Router()
 
-authRouter.post('/registration')
-authRouter.post('login')
-authRouter.get('/users')
+authRouter.post('/registration', authController.registration)
+authRouter.post('login', authController.login)
+authRouter.get('/users', authController.getUsers)
 
 module.exports = authRouter
